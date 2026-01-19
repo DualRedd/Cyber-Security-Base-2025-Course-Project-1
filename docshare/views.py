e570592dd16ac96f99dda841a021db5ec96cd73f
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2026-01-19 18:40:16 +0200

    Fix missing import

--- docshare/views.py
+++ docshare/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Document
 from .forms import DocumentForm
